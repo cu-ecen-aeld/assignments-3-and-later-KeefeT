3ab54e790b11c5be7a7a2c382bd0445c0967d997
Thomas Keefe <tjk819@gmail.com>
2024-10-18 15:41:04 -0600

    adding comments for peer review

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-if [ -z $1 ] || [ -z $2 ] ; then 
-    echo "Params null"
+if [ -z $1 ] || [ -z $2 ] ; then #null check params 1 and 2
+    echo "params null"
     exit 1
-else 
-    filesdir=$1
-    searchstr=$2
-        
-    if [ ! -d $filesdir ] ; then 
-        echo "filesdir : $filesdir is not a dir"
-        exit 1
-    else
-        count=$(find "$filesdir" -type f | wc -l)
-
-        instances=$(grep -r "$searchstr" "$filesdir" | wc -l)
-
-        echo "The number of files are $count and the number of matching lines are $instances"
-    fi;    
 fi;
+
+filesdir=$1
+searchstr=$2
+
+if [ ! -d $filesdir ] ; then # if "$filesdir" is not a directory
+    echo "filesdir : $filesdir is not a dir"
+    exit 1
+fi;    
+
+count=$(find "$filesdir" -type f | wc -l) #get count of files recursively
+
+instances=$(grep -r "$searchstr" "$filesdir" | wc -l) #grep those files recursively
+
+echo "The number of files are $count and the number of matching lines are $instances"
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -z $1 ] || [ -z $2 ] ; then 
+if [ -z $1 ] || [ -z $2 ] ; then #null check params 1 and 2
     echo "Params null"
     exit 1
 fi;
@@ -8,9 +8,9 @@ fi;
 writefile=$1
 writestr=$2
 
-directory=$(dirname "$writefile")
+directory=$(dirname "$writefile") #return dir path from string
 
-if ! mkdir -p "$directory" ; then
+if ! mkdir -p "$directory" ; then #run mkdir to create dir if does not exist; then check if that op failed
     echo "couldnt make dir!"
     exit 1
 fi;
